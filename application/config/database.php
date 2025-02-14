5eb42d9636371e22f8f841049369f9fc092a140b
yadiputra <43856886+yadiputra@users.noreply.github.com>
2025-02-14 14:31:10 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,16 +73,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'ma',
+	'username' => 'anik6619',
+	'password' => 'bjTSD3YuG7hK39',
+	'database' => 'anik6619_ma',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'development'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
