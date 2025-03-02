0453d596cd89070946387a19e48eb1eb27f98f6f
yadiputra <43856886+yadiputra@users.noreply.github.com>
2025-03-02 20:06:47 +0700

    meta

--- application/views/template.php
+++ application/views/template.php
@@ -9,6 +9,8 @@
         <meta content="" name="keywords">
         <meta content="" name="description">
 
+		<meta name="google-site-verification" content="MBvpLMaT-KJGr1Eoo8PoUVdCu8DM9P8k-kUTqz6sqcA" />
+		
         <!-- Google Web Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
